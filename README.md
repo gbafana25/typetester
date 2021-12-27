616b08604dede3eefaa3268fe1a8695cd8a6801f
gbafana25 <garethkm25@gmail.com>
2021-12-26 22:52:35 -0600

    get word from text function works

---
+++ README.md
@@ -0,0 +1,12 @@
+# typetester
+
+Typing practice for the command line on Linux
+
+## Outline
+
+- enable canonical mode in terminal (read by character) 
+- react to space keypress (advance to next word)
+- clear typed word with ansi escape sequence
+
+## TODO
+- read typed chars until space, compare to word in paragraph   
--- main.c
+++ main.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
+#include <string.h>
 
+
+typedef struct {
+	int curr;
+	int start; 
+
+} position; 
+
+position ps;
 
 void setup_terminal() 
 {
@@ -14,16 +23,55 @@ void setup_terminal()
 
 }
 
+char *get_word_from_text(char *t)
+{
+	char *word; 	
+	int len = 0;
+	for(int i = ps.start; i < strlen(t); i++)
+	{
+		if(t[i] == ' ')
+		{
+			len = ps.curr - ps.start;
+			word = malloc(len * sizeof(char));
+			strncpy(word, t+ps.start, len);
+			ps.curr++;
+			ps.start = ps.curr;
+			len = 0;
+			return word;
+		} else {
+			ps.curr++;	
+		}
+
+	}
+
+
+}
+
 
 int main()
 {
 	char test;
+	ps.curr = 0;
+	ps.start = 0;
 	printf("\n\033[31;1mCommand Line Typing Tester\033[0m\n\n");
-	printf("This is me. This is the shape that let others recognize me as myself.\n\n");
+	char *text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+	printf("%s\n", text);
+	printf("%s\n", get_word_from_text(text));
+	printf("%s\n", get_word_from_text(text));
+	printf("%s\n", get_word_from_text(text));
+	printf("%s\n", get_word_from_text(text));
+	printf("%s\n", get_word_from_text(text));
+	printf("%s\n", get_word_from_text(text));
+	printf("%s\n", get_word_from_text(text));
 	setup_terminal();
-	while(read(STDIN_FILENO, &test, 1) == 1 && test != ' '); 
-	write(STDOUT_FILENO, "\e[1K\n", 5);
-	fflush(stdout);
+	while(read(STDIN_FILENO, &test, 1) == 1)
+	{
+		if(test == ' ') {
+			write(STDOUT_FILENO, "\e[1K\n", 5);
+			break;
+		}
+
+	}
 	return 0;
 }
 
